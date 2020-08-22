d87198da0cb2f811bf0613e3ad7308e49cdb9275
cnoriega20 <c79.noriega@gmail.com>
2020-08-22 10:24:39 -0500

    Project structure from Spring Initializr

---
+++ src/main/java/springboot/training/resourceserver/SpringbootResourceServerApplication.java
@@ -0,0 +1,13 @@
+package springboot.training.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootResourceServerApplication.class, args);
+    }
+
+}
